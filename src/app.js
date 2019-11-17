01144e97e6db58e56aed516e1a761082f48cb1f1
Michael Adair <mjadair91@gmail.com>
2019-11-17 19:36:05 +0000

    Updated project

--- src/app.js
+++ src/app.js
@@ -1,18 +1,79 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+import './style.scss'
+
 class App extends React.Component {
 
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      quotesList: []
+    }
   }
 
-  render() {
-    return (
-      <h1>Hello world</h1>
-    )
+
+  componentDidMount() {
+    fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+      .then(resp => resp.json())
+      .then(resp => this.setState({ quotesList: resp }))
   }
+
+
+  render() {
+    if (!this.state.quotesList) {
+      return <div>
+        <section className={'hero'}>
+          <div className={'hero-body'}>
+            <div className={'container'}>
+              <h1 className={'title has-text-centered'}>
+                The Simpsons <br /> Random Quote Generator
+              </h1>
+            </div>
+          </div>
+        </section>
+        <section className={'columns'}>
+          <div className={'column'}>
+            <p>Hold yer horses! The quotes are loading!</p>
+          </div>
+          <div className={'column'}>
+            <p>Seriously! give them a wee moment!</p>
+          </div>
+        </section>
+      </div>
+    }
+    return <div>
+      <section className={'hero'}>
+        <div className={'hero-body'}>
+          <h1 className={'title has-text-centered'}>
+            The Simpsons <br /> Random Quote Generator
+          </h1>
+        </div>
+      </section>
+      <section className={'columns'}>
+        <div className={'column'}>
+          <figure className={'image is-square'}>
+            {this.state.quotesList.map((quote, i) => {
+              return <img key={i} src={quote.image} />
+            })}
+          </figure>
+        </div>
+        <div className={'column has-text-left'}>
+          {this.state.quotesList.map((quote, i) => {
+            return <div className={'has-text-left'} key={i}>" {quote.quote} " <br /> <br /> -{quote.character}</div>
+          })}
+          <br />
+          <button onClick={() => this.componentDidMount()} className={'button is-large is-rounded'}>New Quote!</button>
+        </div>
+      </section>
+    </div>
+
+
+
+  }
+
+
 }
 
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,10 @@ module.exports = {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] }
+      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.(png|jpe?g|gif)$/i, loader: ['file-loader'] }
     ]
+   
   },
   devServer: {
     contentBase: path.resolve('src'),
